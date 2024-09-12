a7ead8bf2684fef1716e9d6ed1dfe5609c448264
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-09-13 00:46:08 +0700

    Update QuestionnaireResultDetail.php

--- app/Models/QuestionnaireResultDetail.php
+++ app/Models/QuestionnaireResultDetail.php
@@ -9,17 +9,17 @@ class QuestionnaireResultDetail extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'questionnaire_result_id',
-        'learning_category_questionnairy_id',
+        'result_id',
+        'category_questionnairy_id',
         'questionnairy_id'
     ];
     public function questionnaireResult()
     {
-        return $this->belongsTo(QuestionnaireResult::class);
+        return $this->belongsTo(QuestionnaireResult::class, 'result_id');
     }
     public function learningCategoryQuestionnairy()
     {
-        return $this->belongsTo(LearningCategoryQuestionnairy::class);
+        return $this->belongsTo(LearningCategoryQuestionnairy::class, 'category_questionnairy_id');
     }
     public function questionnairy()
     {
