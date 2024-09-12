10b4765094c8f0250ce3e39acc0ab3e0e6ddc144
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-09-13 01:19:07 +0700

    Update footer.blade.php

--- resources/views/layouts/backend/footer.blade.php
+++ resources/views/layouts/backend/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
 <footer class="content-footer footer bg-footer-theme">
-    <p class="text-center">Copyright @ 2023</p>
+    <p class="text-center">Copyright 4miraclevibes.com</p>
   </footer>
   <!-- / Footer -->
